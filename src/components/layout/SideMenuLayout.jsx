be527fda2b2ad00ab2055ce536c21a8d1ceef7d9
seyun0714 <seun0714@naver.com>
2025-05-12 10:32:56 +0900

    로그인 페이지 리다이렉트 및 base url 설정

--- src/components/layout/SideMenuLayout.jsx
+++ src/components/layout/SideMenuLayout.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "./Layout.module.scss";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Header from "../header/Header";
 import SideMenu from "../sideMenu/SideMenu";
 
 export default function SideMenuLayout() {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return <Navigate to={"/login"} replace />;
+  }
   return (
     <div className={styles.layoutwrapper}>
       <Header />
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "url";
+import { basename } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
