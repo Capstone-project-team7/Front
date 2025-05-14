38f1c7f3856565ac170ce5ce4a1990f150f11dec
seyun0714 <seun0714@naver.com>
2025-05-14 16:13:39 +0900

    streaming api 변경

--- src/apis/cctvApi.js
+++ src/apis/cctvApi.js
@@ -5,6 +5,6 @@ export const cctvApi = {
   createCctv: (cctvData) => api.post(`/cctv/add`, cctvData, true),
   updateCctv: (id, cctvData) => api.put(`/cctv/update/${id}`, cctvData, true),
   deleteCctv: (ids) => api.delete(`/cctv/delete`, ids, true),
-  startStreaming: (id) => api.get(`/streaming-video/connect/${id}`, {}, true),
-  stopStreaming: (id) => api.get(`/streaming-video/disconnect/${id}`, {}, true),
+  startStreaming: (id) => api.put(`/streaming-video/connect/${id}`, {}, true),
+  stopStreaming: (id) => api.put(`/streaming-video/disconnect/${id}`, {}, true),
 };
