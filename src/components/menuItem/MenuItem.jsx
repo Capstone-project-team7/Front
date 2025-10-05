93077b4faf07aca9c55368c3789eb329204f7289
seyun0714 <seun0714@naver.com>
2025-10-05 15:15:12 +0900

    style: sidemenu 반응형으로 수정

--- src/components/menuItem/MenuItem.jsx
+++ src/components/menuItem/MenuItem.jsx
@@ -1,18 +1,12 @@
-import React, { useEffect, useState } from "react";
-import styles from "./MenuItem.module.scss";
-import { Link, useLocation } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHouseChimney,
-  faCalendar,
-  faVideo,
-  faFileLines,
-  faGear,
-} from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect, useState } from 'react';
+import styles from './MenuItem.module.scss';
+import { Link, useLocation } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouseChimney, faCalendar, faVideo, faFileLines, faGear } from '@fortawesome/free-solid-svg-icons';
 
 const getBasePath = (pathname) => {
-  const segments = pathname.split("/").filter(Boolean);
-  return segments.length > 0 ? `/${segments[0]}` : "/";
+  const segments = pathname.split('/').filter(Boolean);
+  return segments.length > 0 ? `/${segments[0]}` : '/';
 };
 
 export default function MenuItem({ label, to }) {
@@ -22,26 +16,26 @@ export default function MenuItem({ label, to }) {
   const icontype = {
     홈페이지: faHouseChimney,
     캘린더: faCalendar,
-    "CCTV 관리": faVideo,
+    'CCTV 관리': faVideo,
     이용가이드: faFileLines,
     마이페이지: faGear,
   }[label];
 
   const idtype = {
-    홈페이지: "homepage-menu",
-    캘린더: "calendarpage-menu",
-    "CCTV 관리": "cctvpage-menu",
-    이용가이드: "guidepage-menu",
-    마이페이지: "mypage-menu",
+    홈페이지: 'homepage-menu',
+    캘린더: 'calendarpage-menu',
+    'CCTV 관리': 'cctvpage-menu',
+    이용가이드: 'guidepage-menu',
+    마이페이지: 'mypage-menu',
   }[label];
 
   return (
-    <li className={`${styles.menuitem} ${isActive ? styles.selected : ""}`}>
+    <li className={`${styles.menuitem} ${isActive ? styles.selected : ''}`}>
       <Link id={idtype} className={styles.menuitem__link} to={to}>
         <span className={styles.menuitem__link__icon}>
           <FontAwesomeIcon icon={icontype} />
         </span>
-        {label}
+        <span className={styles.menuitem__link__label}>{label}</span>
       </Link>
     </li>
   );
