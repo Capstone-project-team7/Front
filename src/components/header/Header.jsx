9856111a61fd6a2890c2405b65f543ef801b5c9c
seyun0714 <seun0714@naver.com>
2025-10-11 16:16:16 +0900

    feat: header에 이상행동 유형 툴팁 삽입, 모바일 환경 반응형 메뉴 구현

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import styles from './Header.module.scss';
 import Logo from '@assets/images/logo.webp';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,6 +9,8 @@ import { Link, replace, useNavigate } from 'react-router-dom';
 import { userApi } from '@apis/userApi';
 import { toast } from 'react-toastify';
 import { ClipLoader } from 'react-spinners';
+import InfoTooltip from '@components/infoTooltip/InfoTooltip';
+import useOnClickOutside from '../../hooks/useOnClickOutside';
 
 const getBasePath = (pathname) => {
   const segments = pathname.split('/').filter(Boolean);
@@ -20,8 +22,19 @@ export default function Header({}) {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
   const currentPath = getBasePath(location.pathname);
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isTooltipVisible, setIsTooltipVisible] = useState(false);
+  const tooltipRef = useRef(null);
+
+  useEffect(() => {
+    document.body.style.overflow = isMenuOpen ? 'hidden' : 'auto';
+  }, [isMenuOpen]);
+
+  const toggleTooltip = () => {
+    setIsTooltipVisible(!isTooltipVisible);
+  };
+
+  useOnClickOutside(tooltipRef, () => setIsTooltipVisible(false));
 
   const handleMenuToggle = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -56,6 +69,8 @@ export default function Header({}) {
       <Link className={styles.header__logo} to={'/'}>
         <img src={Logo} className={styles.header__logo__image} />
       </Link>
+
+      {/* 데스크톱 화면 */}
       <ul className={styles.header__menu}>
         <li className={`${styles.header__menu__item} ${currentPath === '/' ? styles.active : ''}`}>
           <Link to="/">대시보드</Link>
@@ -74,9 +89,12 @@ export default function Header({}) {
         </li>
       </ul>
       <div className={styles.header__info}>
-        <button className={styles.header__info__button}>
-          <FontAwesomeIcon icon={faCircleQuestion} size="lg" />
-        </button>
+        <div className={styles.header__info__wrapper} ref={tooltipRef}>
+          <button className={styles.header__info__wrapper__button} onClick={toggleTooltip}>
+            <FontAwesomeIcon icon={faCircleQuestion} size="lg" />
+          </button>
+          {isTooltipVisible && <InfoTooltip />}
+        </div>
       </div>
       <div className={styles.header__logout}>
         <button className={styles.header__logout__button} onClick={handleLogout}>
@@ -84,6 +102,7 @@ export default function Header({}) {
         </button>
       </div>
 
+      {/* 태블릿, 모바일 화면 */}
       <button className={styles.header__hamburger} type="button" aria-label="메뉴 토글" onClick={handleMenuToggle}>
         {isMenuOpen ? (
           <FontAwesomeIcon icon={faXmark} size="lg" color="black" />
@@ -94,38 +113,40 @@ export default function Header({}) {
 
       <div className={styles.header__mobileNav}>
         <ul className={styles.header__mobileNav__menu}>
-          <li className={`${styles.header__mobileNav__menu__item} ${currentPath === '/' ? styles.active : ''}`}>
+          <li className={`${styles.header__menu__item} ${currentPath === '/' ? styles.active : ''}`}>
             <Link to="/" onClick={() => setIsMenuOpen(false)}>
               대시보드
             </Link>
           </li>
-          <li className={`${styles.header__mobileNav__menu__item} ${currentPath === '/calendar' ? styles.active : ''}`}>
+          <li className={`${styles.header__menu__item} ${currentPath === '/calendar' ? styles.active : ''}`}>
             <Link to="/calendar" onClick={() => setIsMenuOpen(false)}>
               캘린더
             </Link>
           </li>
-          <li className={`${styles.header__mobileNav__menu__item} ${currentPath === '/cctv' ? styles.active : ''}`}>
+          <li className={`${styles.header__menu__item} ${currentPath === '/cctv' ? styles.active : ''}`}>
             <Link to="/cctv" onClick={() => setIsMenuOpen(false)}>
               CCTV 관리
             </Link>
           </li>
-          <li className={`${styles.header__mobileNav__menu__item} ${currentPath === '/guide' ? styles.active : ''}`}>
+          <li className={`${styles.header__menu__item} ${currentPath === '/guide' ? styles.active : ''}`}>
             <Link to="/guide" onClick={() => setIsMenuOpen(false)}>
               사용가이드
             </Link>
           </li>
-          <li className={`${styles.header__mobileNav__menu__item} ${currentPath === '/mypage' ? styles.active : ''}`}>
+          <li className={`${styles.header__menu__item} ${currentPath === '/mypage' ? styles.active : ''}`}>
             <Link to="/mypage" onClick={() => setIsMenuOpen(false)}>
               마이페이지
             </Link>
           </li>
         </ul>
-        <div className={styles.header__mobileNav__logout}>
-          <button className={styles.header__mobileNav__logout__button} onClick={handleLogout}>
+        <div className={`${styles.header__logout} ${styles.mobile}`}>
+          <button className={styles.header__logout__button} onClick={handleLogout}>
             <FontAwesomeIcon icon={faArrowRightFromBracket} size="lg" color="black" /> <span>로그아웃</span>
           </button>
         </div>
       </div>
+
+      {isMenuOpen && <div className={styles.overlay} onClick={handleMenuToggle}></div>}
 
       {loading && (
         <div className={styles.loader}>
