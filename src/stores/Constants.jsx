052bd3e4c9a0f294898421c75c7ea7eab72a9dc0
seyun0714 <seun0714@naver.com>
2025-10-11 16:15:41 +0900

    feat: 이상행동 유형 상수화

--- src/stores/Constants.jsx
+++ src/stores/Constants.jsx
@@ -9,4 +9,14 @@ const paths = {
   findPassword: '/find-password',
 };
 
-export { paths };
+const types = [
+  { name: '전도', className: 'falling' },
+  { name: '파손', className: 'break' },
+  { name: '방화', className: 'arson' },
+  { name: '흡연', className: 'smoke' },
+  { name: '유기', className: 'abandon' },
+  { name: '절도', className: 'thief' },
+  { name: '폭행', className: 'assault' },
+];
+
+export { paths, types };
