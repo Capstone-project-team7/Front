02a1a59c1b658389d42c2fe78b53ba36717e53e0
seyun0714 <seun0714@naver.com>
2025-10-31 15:53:43 +0900

    refactor: stores 내의 상수값 constants 폴더로 모듈 분리

---
+++ src/constants/behaviorType.js
@@ -0,0 +1,11 @@
+const behaviorTypes = [
+  { name: "전도", className: "falling" },
+  { name: "파손", className: "break" },
+  { name: "방화", className: "arson" },
+  { name: "흡연", className: "smoke" },
+  { name: "유기", className: "abandon" },
+  { name: "절도", className: "thief" },
+  { name: "폭행", className: "assault" },
+];
+
+export { behaviorTypes };
